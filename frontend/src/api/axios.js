5f98026f7636c58008843901292df0ee1dd45aec
Mayank K <mayankgowda83@gamil.com>
2026-05-25 18:23:13 +0530

    Auto create admin and demo data

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://breathe-esg-backend-csqj.onrender.com'
+  baseURL: 'https://breathe-esg-backend-csqj.onrender.com/api/'
 })
 
 export default API
